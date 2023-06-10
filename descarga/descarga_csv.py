f45fd322d1438648fcb851e02cd5b6014d89ac94
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2023-06-10 03:34:58 -0300

    Generador de descarga csv

---
+++ descarga/descarga_csv.py
@@ -0,0 +1,10 @@
+import os
+import requests
+
+url = "https://cdn.buenosaires.gob.ar/datosabiertos/datasets/secretaria-general-y-relaciones-internacionales/ba-obras/observatorio-de-obras-urbanas.csv"
+archivo_csv = "observatorio-de-obras-urbanas.csv"
+ruta_archivo = os.path.join("descarga", archivo_csv)
+
+response = requests.get(url)
+with open(ruta_archivo, "wb") as archivo:
+    archivo.write(response.content)
