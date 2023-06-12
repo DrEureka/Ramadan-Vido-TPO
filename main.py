985e417f92f4274c276f82f847a4edcb279f1c57
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2023-06-12 00:49:31 -0300

    Le damos un poco de color al menu y respuestas.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from gestion_obras.gestionar_obras import GestionarObra
 from descarga.descarga_csv import descargar_archivo
 from dao.modelo_orm import Obra
+import sys
+import time
 import os
 
+def maquina_escribir(texto):
+    for letra in texto:
+        sys.stdout.write(letra)
+        sys.stdout.flush()
+        time.sleep(0.1)  # Ajusta el valor para controlar la velocidad de escritura
+
+    sys.stdout.write('\n')
+
+def print_color(text, color):
+    color_reset = '\033[0m'
+    print(color + text + color_reset)
 
 def menu():
     print("Menú:")
@@ -24,19 +37,23 @@ def main():
         if opcion == "1":
             # llamo a la funcion y descargo
             descargar_archivo()
-            print("Archivo descargado exitosamente.")
+            todoOkVerde = '\033[32m'
+            print_color("Archivo descargado exitosamente.", todoOkVerde)
 
         elif opcion == "2":
             # crear db y verifico si existe
             if GestionarObra.db_existe():
-                print("La base de datos ya está creada. No se realizará la creación nuevamente.")
+                todoOkVerde='\033[32m'
+                print_color("La base de datos ya está creada. No se realizará la creación nuevamente.", todoOkVerde)
+
             else:
                 GestionarObra.conectar_db()
                 GestionarObra.mapear_orm()
                 print("Base de datos creada exitosamente.")
 
         elif opcion == "3":
-            print("Cargando datos por favor espere...")
+            atencionAmarillo= '\033[33m'
+            print_color("Cargando datos a la db por favor espere...", atencionAmarillo)
             # Gestionar obras
             ruta_archivo_csv = os.path.join(os.path.dirname(__file__), "descarga", "observatorio-de-obras-urbanas.csv")
             ruta_archivo_sanitizado = os.path.join("descarga", "sanitizado_observatorio-de-obras-urbanas.csv")
@@ -45,10 +62,13 @@ def main():
                 if not Obra.select().exists():
                     GestionarObra.limpiar_datos(ruta_archivo_csv)
                     GestionarObra.extraer_datos(ruta_archivo_sanitizado)
-                    print("Carga completada.   ")
-                    print("Obras gestionadas exitosamente.")
+                    todoOkVerde = '\033[32m'
+                    print_color("Carga completada.   ", todoOkVerde)
+                    print_color("Obras gestionadas exitosamente.", todoOkVerde)
                 else:
-                    print("La base de datos ya contiene registros. No se realizará la carga de datos.")
+                    errorRojo = '\033[31m'
+                    print_color("La base de datos ya contiene registros. No se realizará la carga de datos.", errorRojo)
+
             except Exception as e:
                 print("Se produjo un error al gestionar las obras:")
                 print(str(e))
@@ -69,11 +89,23 @@ def main():
             #creo una nueva obra test....
             GestionarObra.nueva_obra()
         elif opcion == "6":
+
+
             print("Saliendo del programa.")
+            color = '\033[32m'
+            color_reset = '\033[0m'
+            print("Trabajo Práctico Final Integrador 2023\n")
+            print("Materia: Desarrollo de Sistemas Orientados a Objetos")
+            print("Curso: 1er Año")
+            print("Profesor: Eduardo Iberti")
+            texto = (color + "Programa realizado por: Daniel Ramadan, Vido Viloria, Alessandrio Gabrielle."+color_reset)
+            maquina_escribir(texto)
+
             break
 
         else:
-            print("Opción inválida. Por favor, ingrese una opción válida.")
+            errorRojo = '\033[31m'
+            print_color("Opción inválida. Por favor, ingrese una opción válida.", errorRojo)
 
 
 if __name__ == "__main__":
