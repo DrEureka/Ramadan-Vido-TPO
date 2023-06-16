574d1506600c6c59aa9356f9780f97b100412021
Vidoalessandro <gabriellevido17@gmail.com>
2023-06-15 21:30:18 -0400

    Agregado el archivo de la clase Obra

---
+++ gestion_obras/clase_obra.py
@@ -0,0 +1,31 @@
+from dao import Obra
+
+class Obra():
+    def __init__(self) -> None:
+        pass
+
+    def nuevo_proyecto(self):
+        pass
+
+    def iniciar_contratacion(self):
+        pass
+
+    def adjudicar_obra(self):
+        pass
+
+    def iniciar_obra(self):
+        pass
+    def actualizar_porcentaje_avance(self):
+        pass
+
+    def incrementar_plazo(self):
+        pass
+
+    def incrementar_mano_obra(self):
+        pass
+
+    def finalizar_obra(self):
+        pass
+
+    def rescindir_obra(self):
+        pass
